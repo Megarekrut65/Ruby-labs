44755176c934b32d73af62cae648017f4b079ba0
Megarekrut65 <bandalak28@gmail.com>
2021-09-21 00:31:27 +0300

    Added part 2 lab2

--- Labs/lab2/numers_range.rb
+++ Labs/lab2/numers_range.rb
@@ -1,1 +1,8 @@
-
+printf 'Enter number base:'
+base = gets.chomp.to_s.to_i
+printf 'Enter number of digits of the number:'
+digits = gets.chomp.to_s.to_i
+printf 'Enter number of digits of the number(integer part):'
+int_part = gets.chomp.to_s.to_i
+res = (1 - base**(1 - digits)) * base**int_part
+puts 'Diap=', res.round
