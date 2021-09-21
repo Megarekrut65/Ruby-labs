c9ecd1336271420f9ef8f840ec9aa5bb4bb5fddc
Megarekrut65 <bandalak28@gmail.com>
2021-09-21 12:09:51 +0300

    Update to_decimal.rb

--- Labs/lab2/to_decimal.rb
+++ Labs/lab2/to_decimal.rb
@@ -1,22 +1,24 @@
 # frozen_string_literal: true
-def integer_part_converter(number, system, index)
+def part_converter(number, system, index)
   return '' if number.empty?
 
   str = number[number.length - 1]
   number = number[0, number.length - 1]
   digit = str.to_i
   res = digit * system**index
-  res + integer_part_converter(number, system, index + 1).to_i
-end
-
-def float_part_converter(number, system)
-
-
+  res + part_converter(number, system, index + 1).to_i
 end
 
 def number_converter(number, system)
+  parts = number.split('.')
+  return 0 if parts.size.zero? || parts.size > 2
+  return part_converter(number, system, 0) if parts.size == 1
 
-  integer_part_converter(number,system, 0)
+  first = part_converter(number, system, 0).to_s
+  second = part_converter(number, system, -1).to_s
+  first = '0' if first.empty?
+  first += ".#{second}" unless second.empty?
+  first
 end
 printf 'Enter calculation system:'
 sys = gets.chomp.to_s.to_i
