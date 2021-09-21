f5992a10126965acc985d37441174edfded2df29
Megarekrut65 <bandalak28@gmail.com>
2021-09-21 18:34:18 +0300

    Update to_decimal.rb

--- Labs/lab2/to_decimal.rb
+++ Labs/lab2/to_decimal.rb
@@ -1,27 +1,41 @@
 # frozen_string_literal: true
+def from_letter_to_number(letter, map)
+  map = map.to_a
+  index = map.find_index(letter)
+  return index + 10 unless index.nil?
+
+  nil
+end
+
+def normalize_number(number)
+  index = from_letter_to_number(number, 'A'..'Z')
+  index = from_letter_to_number(number, 'a'..'z') if index.nil?
+  index = number.to_i if index.nil?
+  index
+end
+
 def part_converter(number, system, index)
-  return '' if number.empty?
+  return 0.0 if number.empty?
 
   str = number[number.length - 1]
   number = number[0, number.length - 1]
-  digit = str.to_i
-  res = digit * system**index
-  res + part_converter(number, system, index + 1).to_i
+  digit = normalize_number(str).to_i
+  res = digit * (system**index).to_f
+  index = index >= 0 ? index + 1 : index - 1
+  res + part_converter(number, system, index).to_f
 end
 
 def number_converter(number, system)
   parts = number.split('.')
   return 0 if parts.size.zero? || parts.size > 2
-  return part_converter(number, system, 0) if parts.size == 1
+  return part_converter(parts[0], system, 0) if parts.size == 1
 
-  first = part_converter(number, system, 0).to_s
-  second = part_converter(number, system, -1).to_s
-  first = '0' if first.empty?
-  first += ".#{second}" unless second.empty?
-  first
+  first = part_converter(parts[0], system, 0)
+  second = part_converter(parts[1], system, -1)
+  first + second
 end
 printf 'Enter calculation system:'
 sys = gets.chomp.to_s.to_i
 printf 'Enter number:'
 number = gets.chomp.to_s
-puts number_converter(number, sys)
+puts 'Result: ', number_converter(number, sys)
