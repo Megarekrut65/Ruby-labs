aa65d372dda143f64d5586834a158782814d7c76
Megarekrut65 <bandalak28@gmail.com>
2021-09-21 11:36:58 +0300

    Update from_decimal.rb

--- Labs/lab2/from_decimal.rb
+++ Labs/lab2/from_decimal.rb
@@ -12,14 +12,33 @@ def normalize_number(number)
   str
 end
 
-def print_number(number, system)
+def integer_part_converter(number, system)
   return '' if number.zero?
 
   digit = number % system
-  print_number(number / system, system).to_s + normalize_number(digit).to_s
+  integer_part_converter(number / system, system).to_s + normalize_number(digit).to_s
+end
+
+def float_part_converter(number, system)
+  return '' if number.zero?
+
+  digit = system * number
+  integer = digit.to_i
+  float = digit.modulo(1)
+  normalize_number(integer).to_s + float_part_converter(float, system).to_s
+end
+
+def number_converter(number, system)
+  integer = number.to_i
+  float = number.modulo(1)
+  first = integer_part_converter(integer, system)
+  second = float_part_converter(float, system).to_s
+  first = '0' if first.empty?
+  first += ".#{second}" unless second.empty?
+  first
 end
 printf 'Enter calculation system:'
 sys = gets.chomp.to_s.to_i
 printf 'Enter number:'
-number = gets.chomp.to_s.to_i
-puts print_number(number, sys)
+number = gets.chomp.to_s.to_f
+puts number_converter(number, sys)
