d17a9594c08389c763a4b71132960d7d02c7927c
Megarekrut65 <bandalak28@gmail.com>
2021-11-16 10:41:42 +0200

    Added  lab6(project part 1)

--- Labs/project/main.rb
+++ Labs/project/main.rb
@@ -4,11 +4,14 @@ include ST
 
 
 students = []
-students << Student.new('Marko', true, 0, false, 'School', 'English')
-students << Student.new('Lako', false, 7, true , 'PTU', 'English')
+students << Student.new('Marko', true, 0, false, 'Teacher PTU', 'English')
+students << Student.new('Lako', false, 7, true, 'Teacher PTU', 'English')
 students << Student.new('Wano', true, 1, false, 'PTU', 'German')
 students << Student.new('Purlp', false, 0, false, 'School', 'English')
 students << Student.new('Tom', false, 0, false, 'Lizei', 'Italic')
-students << Student.new('Marie', true, 3, false, 'School', 'Franch')
+students << Student.new('Marie', true, 3, true, 'Teacher PTU', 'Franch')
 
-puts "Need Domitory: ", Student.need_dormitory_count
+puts 'Need Dormitory: ', Student.need_dormitory_count, ''
+puts 'Teachers with experience:', Student.worked_teacher_student_list, ''
+puts 'Finished teacher PTU: ', Student.finished_ptu_student_list, ''
+puts 'Language group list: ', Student.language_group_list
--- Labs/project/student.rb
+++ Labs/project/student.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 module ST
   class Student
     attr_accessor :name, :need_dormitory, :experience, :was_teacher, :what_graduated, :language
@@ -12,9 +13,43 @@ module ST
       @language = language
     end
 
+    def to_s
+      @name
+    end
     def self.need_dormitory_count
       res = 0
       ObjectSpace.each_object(Student) { |s| res += 1 if s.need_dormitory }
+      res
+    end
+
+    def self.worked_teacher_student_list
+      res = []
+      ObjectSpace.each_object(Student) { |s| res << s if s.was_teacher && s.experience >= 2 }
+      res
+    end
+
+    def self.finished_ptu_student_list
+      res = []
+      ObjectSpace.each_object(Student) { |s| res << s if s.what_graduated.downcase == 'teacher ptu' }
+      res
+    end
+
+    def self.language_group_list
+      res = []
+      ObjectSpace.each_object(Student) do |s|
+        i = nil
+        res.each_index do |index|
+          if res[index].include? s.language
+            i = index
+            break
+          end
+        end
+        if !i.nil?
+          res[i] += ", #{s}"
+        else
+          res << "#{res.size + 1}) #{s.language}: #{s}"
+        end
+      end
       res
     end
   end
